29ebd340f60030ed30a87c9089930ed8c9ab6c30
Erick Blanco <a311008@uach.mx>
2019-09-23 22:26:03 -0600

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
+import {DashboardComponent } from '@components/dashboard/dashboard.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'logbook', pathMatch: 'full' },
+  /*{ path: '', component: CleanComponent, children:[
+    { path: 'login', component: LoginComponent },
+  ] },*/
+  { path: '', component: MainComponent,  children: [
+    { path: 'logbook', component: DashboardComponent },
+    // { path: 'logbook', component: DashboardComponent, canActivate : [AuthGuardService] },
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,61 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/main/footer/footer.component';
+import { HeaderComponent } from './layout/main/header/header.component';
+import { MenuComponent } from './layout/main/menu/menu.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatTableModule,
+    MatListModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    FormsModule,
+    HttpClientModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
