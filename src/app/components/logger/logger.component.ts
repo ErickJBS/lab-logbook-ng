3c7baf7e16ddbebb26ccacd300ed52531eccf25f
Erick Blanco <a311008@uach.mx>
2019-10-11 09:51:23 -0600

    logger updates

--- src/app/components/logger/logger.component.ts
+++ src/app/components/logger/logger.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '@services/data.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-logger',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoggerComponent implements OnInit {
 
-  constructor() { }
+  studentId: string;
+  lastStudent: any;
+
+  constructor(
+    private data: DataService,
+    private toast: MessageService,
+  ) { }
 
   ngOnInit() {
   }
 
+  onStudentLog() {
+    this.data.getStudent(this.studentId).subscribe((data: any) => {
+      if (data && data.length > 0) {
+        this.lastStudent = data[0];
+      } else {
+        this.displayMessage('Estudiante no encontrado');
+      }
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  displayMessage(msg: string) {
+    this.toast.add({
+      severity: 'warn', summary: msg
+    });
+  }
 }
